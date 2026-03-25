99cb595bafd59901c8711a487df324dfc4fd1693
Justine Prévost <ju.prevost10@gmail.com>
2026-03-25 15:38:23 +0100

    Clean repo: remove ignored files

--- src/.ipynb_checkpoints/logger_setup-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import logging
-import logging.config
-
-def setup_logging(config):
-        logging.config.dictConfig(config["logging"]) # I created key 'logging' in yaml because it waited for everything not only the loggers
